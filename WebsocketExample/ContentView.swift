99e45d3e5732ebed85c204b980644a981688ed1d
Nghia Tran <vinhnghiatran@gmail.com>
2024-11-29 11:44:40 +0700

    Initial Commit

---
+++ WebsocketExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebsocketExample
+//
+//  Created by Nghia Tran on 29/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebsocketExample/WebsocketExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebsocketExampleApp.swift
+//  WebsocketExample
+//
+//  Created by Nghia Tran on 29/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WebsocketExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
